a62cc2fe4533c641fcad5dc90fb1d30ee0b658e7
Patrick McMurchie <patrick.mcmurchie@gmail.com>
2018-04-10 13:32:46 -0700

    resource/category updates

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='BlastRadius',
-    version='0.1.12',
+    version='0.1.13',
     author='Patrick McMurchie',
     author_email='patrick.mcmurchie@gmail.com',
     packages=find_packages(),
