4d9c369b827532394656d9356b71227e328c2d75
Patrick McMurchie <patrickm@moxiworks.com>
2018-08-07 09:26:02 -0700

    bump version to 0.1.16

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='BlastRadius',
-    version='0.1.15',
+    version='0.1.16',
     author='Patrick McMurchie',
     author_email='patrick.mcmurchie@gmail.com',
     packages=find_packages(),
