1d56a88d1bab81f5d031676c468a61171371ab10
Jon Corbin <jon.corbin8@gmail.com>
2020-09-01 22:25:12 -0400

    handle remote modules

--- blastradius/handlers/terraform.py
+++ blastradius/handlers/terraform.py
@@ -30,45 +30,32 @@ class Terraform:
                     raise RuntimeError('Exception occurred while parsing ' + fname) from e
 
 
-        # then any submodules it may contain, skipping any remote modules for
-        # the time being.
+        # then any submodules it may contain
         self.modules = {}
         if 'module' in self.config:
             for name, mod in [(k, v) for x in self.config['module'] for (k, v) in x.items()]:
                 if 'source' not in mod:
                     continue
                 source = mod['source'][0]
-                # '//' used to refer to a subdirectory in a git repo
-                if re.match(r'.*\/\/.*', source):
-                    continue
-                # '@' should only appear in ssh urls
-                elif re.match(r'.*\@.*', source):
-                    continue
-                # 'github.com' special behavior.
-                elif re.match(r'github\.com.*', source):
-                    continue
-                # points to new TFE module registry
-                elif re.match(r'app\.terraform\.io', source):
-                    continue
-                # bitbucket public and private repos
-                elif re.match(r'bitbucket\.org.*', source):
-                    continue
-                # git::https or git::ssh sources
-                elif re.match(r'^git::', source):
-                    continue
-                # git:// sources
-                elif re.match(r'^git:\/\/', source):
-                    continue
-                # Generic Mercurial repos
-                elif re.match(r'^hg::', source):
-                    continue
-                # Public Terraform Module Registry
-                elif re.match(r'^[a-zA-Z0-9\-_]+\/[a-zA-Z0-9\-_]+\/[a-zA-Z0-9\-_]+', source):
-                    continue
-                # AWS S3 buckets
-                elif re.match(r's3.*\.amazonaws\.com', source):
-                    continue
-                self.modules[name] = Terraform(directory=os.path.join(self.directory, source), settings=mod)
+                
+                path = os.path.join(self.directory, source)
+                if os.path.exists(path):
+                    # local module
+                    self.modules[name] = Terraform(directory=path, settings=mod)
+                else:
+                    # remote module
+                    # Since terraform must be init'd before use, we can
+                    # assume remote modules have been downloaded to .terraform/modules
+                    path = os.path.join(os.getcwd(), '.terraform', 'modules', name)
+                    
+                    # Get the subdir if any
+                    match = re.match(r'.*(\/\/.*)(?!:)', source)
+                    if re.match(r'.*\/(\/.*)(?!:)', source):
+                        path = os.path.join(path, match.groups()[0])
+                    
+                    self.modules = Terraform(directory=path, settings=mod)
+
+                
 
 
     def get_def(self, node, module_depth=0):
