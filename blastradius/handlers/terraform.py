3c27bda825feb250c095a925b89bbcf8cd5db90a
Olivier Bourdon <olivierbourdon38@gmail.com>
2020-12-08 17:20:49 +0100

    Update to HCL2

--- blastradius/handlers/terraform.py
+++ blastradius/handlers/terraform.py
@@ -5,7 +5,7 @@ import os
 import re
 
 # 3rd party libraries
-import hcl    # hashicorp configuration language (.tf)
+import hcl2    # hashicorp configuration language (.tf)
 
 class Terraform:
     """Finds terraform/hcl files (*.tf) in CWD or a supplied directory, parses
@@ -23,7 +23,7 @@ class Terraform:
             with open(fname, 'r', encoding='utf-8') as f:
                 self.config_str += f.read() + ' '
         config_io = io.StringIO(self.config_str)
-        self.config = hcl.load(config_io)
+        self.config = hcl2.load(config_io)
 
         # then any submodules it may contain, skipping any remote modules for
         # the time being.
