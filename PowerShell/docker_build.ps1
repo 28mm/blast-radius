41f1702d65d0392993e8879adc7ac449a67e5616
Ian Liu <liuusa.tw@gmail.com>
2022-08-16 15:45:27 -0400

    Update PowerShell Script to Avoid Build Error
    Moving the PowerShell Script to the PowerShell folder means the Dockerfile is no longer in the same relative location, so I updated it to refer back to the parent directory.

--- PowerShell/docker_build.ps1
+++ PowerShell/docker_build.ps1
@@ -1,4 +1,4 @@
 & docker buildx build -f ..\Dockerfile `
 --platform `
 linux/arm64,linux/amd64,linux/amd64/v2,linux/ppc64le,linux/s390x,linux/386,linux/arm/v7,linux/arm/v6 `
--t blast-radius-fork .
+-t blast-radius-fork ../
