e1d3cfd623094d01daac0cb645f26d59aa89de8f
Ian Liu <liuusa.tw@gmail.com>
2022-08-18 15:40:21 -0400

    Enable Building Docker Images for Other Architectures
    Docker Buildx can only build images and cache them, or push them to a remote repository. There is no option of locally storing them. Therefore, we build the image and then pull it back down after pushing.

--- BASH/docker_build.sh
+++ BASH/docker_build.sh
@@ -2,7 +2,7 @@
 IMAGE_NAME="blast-radius-fork-local"
 MULTI_CPU=false
 SCRIPT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")") # get the directory of this script
-DOCKERFILE_DIR=$(dirname -- "$(readlink -f -- "$SCRIPT_DIR")")
+DOCKERFILE_DIR=$(dirname -- "$(readlink -f -- "$SCRIPT_DIR")") # get the parent directory of this script (the directory of the Dockerfile)
 
 if [ "$1" != "" ]; then
   IMAGE_NAME=$1
@@ -18,10 +18,12 @@ if [ ! -e "$DOCKERFILE_DIR/Dockerfile" ]; then
 fi
 
 if [ "$MULTI_CPU" == false ]; then
-  echo "Building image $IMAGE_NAME without multi-cpu support"
+  echo "Building image $IMAGE_NAME without multi-cpu support. Your image will be saved locally."
   docker build -t "$IMAGE_NAME" "$DOCKERFILE_DIR"
 else
-  #FIXME: multi-cpu build is not working yet
-  echo "Building image $IMAGE_NAME with multi-cpu support"
-  docker buildx build --platform linux/arm64,linux/amd64,linux/amd64/v2,linux/ppc64le,linux/s390x,linux/386,linux/arm/v7,linux/arm/v6 -t "$IMAGE_NAME" "$DOCKERFILE_DIR"
+  echo "Building image $IMAGE_NAME with multi-cpu support. Your image will be pushed remotely to Docker Hub and saved locally afterwards. "
+  docker buildx build \
+  --platform linux/arm64,linux/amd64,linux/amd64/v2,linux/ppc64le,linux/s390x,linux/386,linux/arm/v7,linux/arm/v6 \
+  -t "$IMAGE_NAME" --push "$DOCKERFILE_DIR"
+  docker pull "$IMAGE_NAME"
 fi
