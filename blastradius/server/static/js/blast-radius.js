4b7127d035021101c6700c4927631d545aa6327e
Ian Liu <liuusa.tw@gmail.com>
2022-08-25 09:30:48 -0400

    Removed redundant code

--- blastradius/server/static/js/blast-radius.js
+++ blastradius/server/static/js/blast-radius.js
@@ -373,11 +373,7 @@ blastradius = function (selector, svg_url, json_url, br_state = {}, uploadXML = 
                 });
             // } else {
             //     console.error("An error occurred when parsing JSON data for resource descriptions");
-            //     let div = document.createElement("div");
-            //     div.setAttribute("role", "alert");
-            //     div.classList = ["alert alert-danger"];
-            //     div.textContent = "A server exception has occurred. The graph is still usable but without all features enabled such as filtering content"
-            //     document.getElementsByClassName("navbar")[0].appendChild(div);
+            // alert("A server exception has occurred. The graph is still usable but without all features enabled such as filtering content")
             //     edges = []
             // }
 
