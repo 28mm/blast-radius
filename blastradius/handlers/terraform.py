1b1c0aa3f5df7a2519d9d67a39166bccf8a1352a
Brett Lentz <blentz@users.noreply.github.com>
2022-08-29 11:56:02 -0400

    Avoid infinite recursions in terraform parser

--- blastradius/handlers/terraform.py
+++ blastradius/handlers/terraform.py
@@ -18,7 +18,7 @@ class Terraform:
         self.settings = settings if settings else {}
 
         # handle the root module first...
-        self.directory = directory if directory else os.getcwd()
+        self.directory = os.path.abspath(directory) if directory else os.getcwd()
         #print(self.directory)
         self.config_str = ''
         iterator = iglob( self.directory + '/*.tf')
@@ -66,6 +66,9 @@ class Terraform:
                 # AWS S3 buckets
                 elif re.match(r's3.*\.amazonaws\.com', source):
                     continue
+
+                if source == '.':
+                    continue   # avoid infinite recursion
 
                 path = os.path.join(self.directory, source)
                 if os.path.exists(path):
