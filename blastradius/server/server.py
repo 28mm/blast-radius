0a3f4ed7b2d27f458606a4cac49071e275067c99
Ian Liu <liuusa.tw@gmail.com>
2022-10-20 01:24:05 -0400

    Better Error Handling
    Original Idea from obourdon/blast-radius

--- blastradius/server/server.py
+++ blastradius/server/server.py
@@ -197,9 +197,9 @@ def graph_json():
 
 
 def run_tf_graph():
-    completed = subprocess.run(['terraform', 'graph'], stdout=subprocess.PIPE)
+    completed = subprocess.run(['terraform', 'graph'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if completed.returncode != 0:
-        raise
+        raise Exception('Execution error', completed.stderr)
     return completed.stdout.decode('utf-8')
 
 
